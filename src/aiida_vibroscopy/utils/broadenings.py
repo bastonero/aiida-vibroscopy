7cca54523cc117a95586ad6f261d10791bb0987c
bastonero <bastonero.lorenzo@gmail.com>
2023-07-26 13:23:58 +0000

    Utils: add Gaussian and Voigt broadening
    More broadening/smearing functions are added;
    depending on the experimental setup, the Lorentzian
    function may not be sufficient to accurately describe
    the experimental features, such as the laser FWHM.

--- src/aiida_vibroscopy/utils/broadenings.py
+++ src/aiida_vibroscopy/utils/broadenings.py
@@ -2,10 +2,12 @@
 """Broadening functions for plottin spectra."""
 from __future__ import annotations
 
+from copy import deepcopy
+
 import numpy as np
 
 
-def lorentz(x_range: np.ndarray, peak: float, intensity: float, sigma: float):
+def lorentz(x_range: np.ndarray, peak: float, intensity: float, gamma: float):
     """Compute a Lorentzian function.
 
     .. note:: assuming all quantities in cm-1, but intensity at arb. units
@@ -13,14 +15,14 @@ def lorentz(x_range: np.ndarray, peak: float, intensity: float, sigma: float):
     :param x_range: frequency range
     :param peak: peak position
     :param intensity: intensity of the peak
-    :param sigma: broadening, full width at half maximum (FWHM)
+    :param gamma: broadening, full width at half maximum (FWHM)
     :return: :class:`numpy.ndarray`
     """
-    gamma = sigma / 2.0
-    return intensity * gamma / ((x_range - peak)**2 + gamma**2) / np.pi
+    hwhm = gamma / 2.0
+    return intensity * hwhm / ((x_range - peak)**2 + hwhm**2) / np.pi
 
 
-def multilorentz(x_range: np.ndarray, peaks: list[float], intensities: list[float], sigmas: float | list[float]):
+def multilorentz(x_range: np.ndarray, peaks: list[float], intensities: list[float], gammas: float | list[float]):
     """Compute Lorentzian function for multiple peaks, and sum it.
 
     .. note:: assuming all quantities in cm-1, but intensities at arb. units
@@ -28,18 +30,19 @@ def multilorentz(x_range: np.ndarray, peaks: list[float], intensities: list[floa
     :param x_range: frequency range
     :param peaks: list of peak positions
     :param intensities: list of intensities for each position
-    :param sigmas: list of broadenings, i.e. full width at half maximum (FWHM)
+    :param gammas: list of broadenings, i.e. full width at half maximum (FWHM)
     :return: :class:`numpy.ndarray`
     """
     tot = np.zeros(np.array(x_range).shape)
 
-    if isinstance(sigmas, float):
-        sigmas = [sigmas for _ in peaks]
-    elif isinstance(sigmas, list):
-        if len(sigmas) != len(peaks):
-            raise ValueError("length of `sigmas` and `peaks` doesn't match")
+    if isinstance(gammas, float):
+        sigmas = [gammas for _ in peaks]
+    elif isinstance(gammas, (list, np.ndarray)):
+        if len(gammas) != len(peaks):
+            raise ValueError("length of `gammas` and `peaks` doesn't match")
+        sigmas = deepcopy(gammas)
     else:
-        sigmas = float(sigmas)
+        sigmas = float(gammas)
 
     if len(intensities) != len(peaks):
         raise ValueError("length of `intensities` and `peaks` doesn't match")
@@ -47,3 +50,136 @@ def multilorentz(x_range: np.ndarray, peaks: list[float], intensities: list[floa
     for peak, intensity, sigma in zip(peaks, intensities, sigmas):
         tot += lorentz(x_range, peak, intensity, sigma)
     return tot
+
+
+def voigt_profile(x_range: np.ndarray, peak: float, intensity: float, gamma_lorentz: float, sigma_gaussian: float):
+    """Compute a Voigt profile convolution.
+
+    .. note:: assuming all quantities in cm-1, but intensities at arb. units;
+        implementation as from `Ida et al. J. Appl. Cryst. (2000). 33, 1311`
+
+    .. important:: FWHM_Gaussian = 2 * sqrt(2 * ln(2)) * sigma_gaussian;
+        FWHM_Gaussian ~ 2.355 * sigma_gaussian
+
+    :param x_range: frequency range
+    :param peak: peak position
+    :param intensity: intensity of the peak
+    :param gamma_lorentz: Lorentzian broadening, full width at half maximum (FWHM)
+    :param sigma_gaussian: Guassian broadening, corresponding to the deviation standard (not FWHM)
+    :return: :class:`numpy.ndarray`
+    """
+    # f_L = FWHM parameter for Lorenztian
+    # f_G = FWHM parameter for Gaussian
+    f_L = deepcopy(gamma_lorentz)
+    f_G = 2.0 * np.sqrt(2 * np.log(2.0)) * deepcopy(sigma_gaussian)
+    rho = f_L / (f_G + f_L)
+    x = x_range - peak
+    #
+    list_a = [0.66000, 0.15021, -1.24984, 4.74052, -9.48291, 8.48252, -2.95553]
+    list_b = [-0.42179, -1.25693, 10.30003, -23.45651, 29.14158, -16.50453, 3.19974]
+    list_c = [1.19913, 1.43021, -15.36331, 47.06071, -73.61822, 57.92559, -17.80614]
+    list_d = [1.10186, -0.47745, -0.68688, 2.76622, -4.55466, 4.05475, -1.26571]
+    list_f = [-0.30165, -1.38927, 9.31550, -24.10743, 34.96491, -21.18862, 3.70290]
+    list_g = [0.25437, -0.14107, 3.23653, -11.09215, 22.10544, -24.12407, 9.76947]
+    list_h = [1.01579, 1.50429, -9.21815, 23.59717, -39.71134, 32.83023, -10.02142]
+    #
+    w_G = 1.
+    w_L = 1.
+    w_I = 0.
+    w_P = 0.
+    eta_L = rho
+    eta_I = 0.
+    eta_P = 0.
+    #
+    for index, _ in enumerate(list_a):
+        i = index  #fortran convention
+        w_G = w_G - rho * list_a[index] * (rho**i)
+        w_L = w_L - (1. - rho) * list_b[index] * (rho**i)
+        w_I = w_I + list_c[index] * (rho**i)
+        w_P = w_P + list_d[index] * (rho**i)
+        eta_L = eta_L + rho * (1. - rho) * list_f[index] * (rho**i)
+        eta_I = eta_I + rho * (1. - rho) * list_g[index] * (rho**i)
+        eta_P = eta_P + rho * (1. - rho) * list_h[index] * (rho**i)
+    #
+    #
+    big_W_G = w_G * (f_G + f_L)
+    big_W_L = w_L * (f_G + f_L)
+    big_W_I = w_I * (f_G + f_L)
+    big_W_P = w_P * (f_G + f_L)
+    #
+    gamma_G = big_W_G / (2.0 * (np.log(2.0)**0.5))
+    gamma_L = big_W_L / 2.
+    gamma_I = big_W_I / (2. * (2**(2. / 3.) - 1.)**0.5)
+    #this parameter has been checked numerically, the original paper has a typo
+    # in the formula for gamma_P
+    gamma_P = big_W_P / (2. * (np.log(np.sqrt(2.0) + 1.0)))
+    #
+    #this because the paper uses a rescaled sigma.
+    res_Gauss = np.sqrt(1.0 / np.pi) * (1.0 / gamma_G) * np.exp(-(x / gamma_G)**2)
+    #
+    res_Lorentzian = (1. / np.pi) * gamma_L / (x**2 + gamma_L**2)
+    #
+    res_f_I = 1. / (2. * gamma_I) * ((1. + (x / gamma_I)**2)**(-3. / 2.))
+    #
+    res_f_P = 1. / (2. * gamma_P) * (2. / (np.exp(x / gamma_P) + np.exp(-x / gamma_P)))**2
+    #
+    res = (1. - eta_L - eta_I - eta_P) * res_Gauss + eta_L * res_Lorentzian + eta_I * res_f_I + eta_P * res_f_P
+    return intensity * res
+
+
+def multilvoigt(
+    x_range: np.ndarray,
+    peaks: list[float],
+    intensities: list[float],
+    gammas_lorentz: float | list[float],
+    sigma_gaussian: float,
+):
+    """Compute Lorentzian function for multiple peaks, and sum it.
+
+    .. note:: assuming all quantities in cm-1, but intensities at arb. units
+
+    .. important:: FWHM_Gaussian = 2 * sqrt(2 * ln(2)) * sigma_gaussian;
+        FWHM_Gaussian ~ 2.355 * sigma_gaussian
+
+    :param x_range: frequency range
+    :param peaks: list of peak positions
+    :param intensities: list of intensities for each position
+    :param gammas_lorentz: list (or single value) of Lorentzian broadenings, i.e. full width at half maximum (FWHM)
+    :param sigma_gaussian: Guassian broadening, corresponding to sigma
+    :return: :class:`numpy.ndarray`
+    """
+    tot = np.zeros(np.array(x_range).shape)
+
+    if isinstance(gammas_lorentz, float):
+        sigmas = [gammas_lorentz for _ in peaks]
+    elif isinstance(gammas_lorentz, (list, np.ndarray)):
+        if len(gammas_lorentz) != len(peaks):
+            raise ValueError("length of `gammas_lorentz` and `peaks` doesn't match")
+        sigmas = deepcopy(gammas_lorentz)
+    else:
+        sigmas = float(gammas_lorentz)
+
+    if len(intensities) != len(peaks):
+        raise ValueError("length of `intensities` and `peaks` doesn't match")
+
+    for peak, intensity, sigma in zip(peaks, intensities, sigmas):
+        tot += voigt_profile(x_range, peak, intensity, sigma, sigma_gaussian)
+    return tot
+
+
+def gaussian(x_range: np.ndarray, peak: float, intensity: float, sigma: float):
+    """Compute a Gaussian function.
+
+    .. note:: assuming all quantities in cm-1, but intensity at arb. units
+
+    .. important:: FWHM_Gaussian = 2 * sqrt(2 * ln(2)) * sigma_gaussian;
+        FWHM_Gaussian ~ 2.355 * sigma_gaussian
+
+    :param x_range: frequency range
+    :param peak: peak position
+    :param intensity: intensity of the peak
+    :param sigma: standard deviation of the Gaussian
+    :return: :class:`numpy.ndarray`
+    """
+    prefactor = intensity / (np.sqrt(2 * np.pi) * sigma)
+    return prefactor * np.exp(-(x_range - peak)**2 / (2 * sigma**2))
--- tests/utils/test_broadening.py
+++ tests/utils/test_broadening.py
@@ -25,14 +25,35 @@ def generate_lorentz_inputs():
     return _generate_lorentz_inputs
 
 
-def test_lorentz(generate_lorentz_inputs):
+@pytest.fixture
+def generate_voigt_inputs():
+    """Generate an input for Voigt profile."""
+
+    def _generate_voigt_inputs(multi=False, sigma_gaussian=1.0):
+        """Generate the inputs."""
+        x_range = np.arange(0, 100, 0.1)
+        peak = 50.0
+        intensity = 1.0
+        sigma = 10.0
+
+        if multi:
+            peak = [20.0, 30.0, 40.0]
+            sigma = peak
+            intensity = peak
+
+        return [x_range, peak, intensity, sigma, sigma_gaussian]
+
+    return _generate_voigt_inputs
+
+
+def test_lorentz_inputs(generate_lorentz_inputs):
     """Test the `lorentz` function."""
     from aiida_vibroscopy.utils.broadenings import lorentz
 
     lorentz(*generate_lorentz_inputs())
 
 
-def test_multilorentz(generate_lorentz_inputs):
+def test_multilorentz_inputs(generate_lorentz_inputs):
     """Test the `multilorentz` function."""
     from aiida_vibroscopy.utils.broadenings import multilorentz
 
@@ -51,7 +72,7 @@ def test_multilorentz_error(generate_lorentz_inputs):
     inputs = generate_lorentz_inputs(multi=True)
     inputs[-1] = [8.0]
 
-    match = r"length of `sigmas` and `peaks` doesn't match"
+    match = r"length of `gammas` and `peaks` doesn't match"
     with pytest.raises(ValueError, match=match):
         multilorentz(*inputs)
 
@@ -61,3 +82,69 @@ def test_multilorentz_error(generate_lorentz_inputs):
     match = r"length of `intensities` and `peaks` doesn't match"
     with pytest.raises(ValueError, match=match):
         multilorentz(*inputs)
+
+
+def test_voigt_against_lorentz(generate_voigt_inputs, generate_lorentz_inputs):
+    """Test the `voigt_profile` function against Lorentz.
+
+    When `sigma_gaussian = 0`, Voigt and Lorentz coincide.
+    """
+    from aiida_vibroscopy.utils.broadenings import lorentz, voigt_profile
+
+    voigt_res = voigt_profile(*generate_voigt_inputs(sigma_gaussian=0))
+    lorentz_res = lorentz(*generate_lorentz_inputs())
+
+    assert np.abs(voigt_res - lorentz_res).max() < 1e-10
+
+
+def test_voigt_against_gaussian(generate_voigt_inputs, generate_lorentz_inputs):
+    """Test the `voigt_profile` function against Gaussian.
+
+    When `sigma_lorentz = 0`, Voigt and Gaussian coincide.
+    """
+    from aiida_vibroscopy.utils.broadenings import gaussian, voigt_profile
+
+    x_range = np.arange(0, 100, 0.1)
+    peak, intensity, sigma = 50, 1, 2
+    voigt_res = voigt_profile(x_range, peak, intensity, gamma_lorentz=0, sigma_gaussian=sigma)
+    gaussian_res = gaussian(x_range, peak, intensity, sigma)
+
+    assert np.abs(voigt_res - gaussian_res).max() < 1e-10
+
+
+def test_voigt_inputs(generate_voigt_inputs):
+    """Test the `voigt_profile` function."""
+    from aiida_vibroscopy.utils.broadenings import voigt_profile
+
+    voigt_profile(*generate_voigt_inputs())
+
+
+def test_multilorentz_inputs(generate_voigt_inputs):
+    """Test the `multilorentz` function."""
+    from aiida_vibroscopy.utils.broadenings import multilvoigt
+
+    multilvoigt(*generate_voigt_inputs(multi=True))
+
+    # Testing sigmas as single float, i.e. same for each peak
+    inputs = generate_voigt_inputs(multi=True)
+    inputs[-1] = 8.0
+    multilvoigt(*inputs)
+
+
+def test_multilvoigt_error(generate_voigt_inputs):
+    """Test `multilvoigt` raising error."""
+    from aiida_vibroscopy.utils.broadenings import multilvoigt
+
+    inputs = generate_voigt_inputs(multi=True)
+    inputs[-2] = [8.0]
+
+    match = r"length of `gammas_lorentz` and `peaks` doesn't match"
+    with pytest.raises(ValueError, match=match):
+        multilvoigt(*inputs)
+
+    inputs = generate_voigt_inputs(multi=True)
+    inputs[-3] = [8.0]
+
+    match = r"length of `intensities` and `peaks` doesn't match"
+    with pytest.raises(ValueError, match=match):
+        multilvoigt(*inputs)
