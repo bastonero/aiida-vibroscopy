2405c0a520f2ecf55f4d11c31582f9efde7b4f4b
Lorenzo <79980269+bastonero@users.noreply.github.com>
2024-12-20 00:58:35 +0100

    Fix pre-commit

--- src/aiida_vibroscopy/utils/broadenings.py
+++ src/aiida_vibroscopy/utils/broadenings.py
@@ -98,7 +98,7 @@ def voigt_profile(x_range: np.ndarray, peak: float, intensity: float, gamma_lore
     eta_I = 0.
     eta_P = 0.
     #
-    for index, i in enumerate(list_a):  # pylint: disable=unnecessary-list-index-lookup
+    for index, i in enumerate(list_a):
         i = index  #fortran convention
         w_G = w_G - rho * list_a[index] * (rho**i)
         w_L = w_L - (1. - rho) * list_b[index] * (rho**i)
